a6f8d28e8d9034a649ed6f4806a52f3e0e0e3cbd
DaniloDecarli <danilosdecarli@gmail.com>
2024-08-29 11:13:35 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Monitor-Temp-Arduino
+
+Usando Thingspeak e CallMeBot;
+
+cp secrets-example.h secrets.h 
+
+Bibliotecas:
+UrlEncode by masayuki
+ThingSpeak by MathWorks
+Adafruit Unified Sensor by Adafruit
+DHT sensor library by Adafruit
+
+
+https://arduino.esp8266.com/stable/package_esp8266com_index.json
+
+Used Model: NodeMCU 1.0
---
+++ secrets-example.h
@@ -0,0 +1,12 @@
+#define SECRET_SSID "SSID"
+#define SECRET_PASS "SENHA"
+
+#define SECRET_CH_ID 1212121212
+#define SECRET_WRITE_APIKEY "APIKEY"
+
+#define WP_PHONE "551111313213123"
+#define WP_KEY "12121212"
+
+#define ACCESS_TOKEN  "112iasdjijsiadijasidj"
+#define ROOM_ID "!IikTHyBLs:server.com.br"
+#define URL = "https://server.com.br/server/client/r0/rooms/$roomId/send/m.room.message";
